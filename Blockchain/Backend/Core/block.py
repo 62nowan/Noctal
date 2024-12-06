d5482504fa08a0af6744bbd1999154d1e93c95c0
shash <34834756+shash64@users.noreply.github.com>
2024-12-06 17:41:11 +0100

    Add documentation block.py

--- Blockchain/Backend/Core/block.py
+++ Blockchain/Backend/Core/block.py
@@ -1,10 +1,17 @@
+"""
+La classe block est un conteneur qui permet de stocker des informations spécifiques dans une chaine.
+
+    - Height (int): La hauteur du bloc dans la chaîne.
+    - BlockSize (int): La taille du bloc en octets.
+    - BlockHeader (objet cf -> blockheader.py): L'entête du bloc contenant d'autres données essentielles (comme le hash précédent, 
+                                                le merkle root, le timestamp, ...)
+    - TxCount (int): Le nombre total de transactions dans ce bloc.
+    - Txs (list): La liste des transactions contenues dans ce bloc.
+"""
 class Block:
-    """
-    Block is a storage container that stores transactions
-    """
     def __init__(self, Height, BlockSize, BlockHeader, TxCount, Txs):
         self.Height = Height                                              
         self.BlockSize = BlockSize                                       
-        self.BlockHeader = BlockHeader                                    
+        self.BlockHeader = BlockHeader                                  
         self.TxCount = TxCount                                           
-        self.Txs = Txs                                                  
+        self.Txs = Txs                                             
