93ab68f926200edd413e1bfc81bd3090388b30be
shash <34834756+shash64@users.noreply.github.com>
2025-03-22 13:07:04 +0100

    Update blockchain.py for GUI

--- Blockchain/Backend/Core/blockchain.py
+++ Blockchain/Backend/Core/blockchain.py
@@ -9,6 +9,7 @@ from Blockchain.Backend.Core.database.database import BlockchainDB, NodeDB
 from Blockchain.Backend.Core.tx import CoinbaseTx
 from multiprocessing import Process, Manager
 from Blockchain.Frontend.run import main
+from Blockchain.Frontend.Gui.NoctalCore import mainGui
 from Blockchain.Backend.Core.network.syncManager import syncManager
 import time
 
@@ -160,9 +161,13 @@ if __name__ == "__main__":
     with Manager() as manager:
         utxos = manager.dict()
         MemPool = manager.dict()
-
-        webapp = Process(target = main, args = (utxos, MemPool, webport))
-        webapp.start()
+        
+        gui = Process(target=mainGui, args=(utxos, MemPool, webport))
+        gui.start()
+        
+        # If you want to  try the Noctal Explorer, disable the gui.
+        #webapp = Process(target = main, args = (utxos, MemPool, webport))
+        #webapp.start()
 
         """Start Server and Listen for miner requests"""
         sync = syncManager(localHost, localHostPort)
